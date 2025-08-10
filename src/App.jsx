984bcb97e55b40f21c57dda77997f92851e087f0
cyber <ojeikelvin@gmail.com>
2025-08-10 01:31:57 +0100

    worked on my part 3 section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import padsData from "./pads.js";
 import React from "react";
-
+import Pad from "./Pad.jsx";
 export default function App() {
+  const [pads, setPads] = React.useState(padsData);
+
+  const buttonElements = pads.map((pad) => (
+    <Pad color={pad.color} on={pad.on} />
+  ));
+
   return (
-    <>
-      <h1>App Component</h1>
-    </>
+    <main>
+      <div className="pad-container">{buttonElements}</div>
+    </main>
   );
 }
+
+/**
+ * Challenge part 2:
+ * 1. Create a separate component called "Pad" and
+ *    replace the `button` above with our <Pad /> component
+ * 2. Pass the Pad component a prop called `color` with the
+ *    value of the same name from the `padsData` objects
+ * 3. In the Pad component, apply an inline style to the <button>
+ *    to set the backgroundColor of the button.
+ *
+ * (We'll deal with the "on" property soon)
+ */
---
+++ src/Pad.jsx
@@ -0,0 +1,10 @@
+export default function Pad(props) {
+  return (
+    <>
+      <button
+        style={{ backgroundColor: props.color }}
+        className={props.on ? "on" : null}
+      ></button>
+    </>
+  );
+}
